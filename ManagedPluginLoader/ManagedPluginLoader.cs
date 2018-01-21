97b37ac6d0ff10dca93b80102fe3b280029d81cd
etormadiv <etormadiv@gmail.com>
2018-01-21 23:26:36 +0000

    Beta 0.1

--- ManagedPluginLoader/ManagedPluginLoader.cs
+++ ManagedPluginLoader/ManagedPluginLoader.cs
@@ -1,12 +1,10 @@
-﻿using System;
+﻿using ManagedPluginContracts;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Windows.Forms;
-using Microsoft.CSharp;
-using ManagedPluginContracts;
 
 namespace ManagedPluginLoader
 {
@@ -16,13 +14,6 @@ namespace ManagedPluginLoader
         private IntPtr pluginsMenuPtr = IntPtr.Zero;
         private List<OllydbgMenu> menuList = new List<OllydbgMenu>();
 
-        public int test(int ptable, string name, uint index, int mode)
-        {
-            //File.WriteAllText("test_function.txt", ptable + " --- " + name);
-
-            return 1;
-        }
-
         public ManagedPluginLoader()
         {
 
@@ -31,9 +22,7 @@ namespace ManagedPluginLoader
         public void LoadManagedPlugins()
         {
             DirectoryInfo di = Directory.CreateDirectory(".\\ManagedPlugins");
-
             menuList.Clear();
-
             foreach (FileInfo file in di.GetFiles())
             {
                 if(file.Name.ToLower().EndsWith(".dll"))
@@ -41,22 +30,17 @@ namespace ManagedPluginLoader
                     try
                     {
                         Assembly asm = AppDomain.CurrentDomain.Load(File.ReadAllBytes(file.FullName));
-                        
                         Type[] exportTypes = asm.GetExportedTypes();
-                        
                         foreach (var item in exportTypes)
                         {
                             if (typeof(IOllydbgPlugin).IsAssignableFrom(item))
                             {
-                                MessageBox.Show("Done");
-
                                 IOllydbgPlugin plugin = (IOllydbgPlugin) Activator.CreateInstance(item);
                                 OllydbgMenu menu = plugin.GetPluginMenu();
                                 menuList.Add(menu);
                                 break; //Get only one type from each assembly, to make things clear
                             }
                         }
-                        
                     }
                     catch (Exception ex)
                     {
